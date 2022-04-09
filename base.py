4a1158ab480ba6e2bdd7b130a0f3e5a0424f5866
DoomSkull <dfordoomskull@gmail.com>
2022-04-09 23:38:57 +0100

    Setting up for general bot code

---
+++ base.py
@@ -0,0 +1,21 @@
+import discord
+
+TOKEN = open("token.txt", "r").read()
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel[:10]})')
+
+
+client.run(TOKEN)
