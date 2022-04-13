ff9b6709a2e8e51167caa3b4c408fd8dce8210d3
DoomSkull <dfordoomskull@gmail.com>
2022-04-13 16:08:53 +0100

    made normal commands work by changing the on_message to call bot to process commands, slash still not working

--- base.py
+++ base.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 
 TOKEN = open("token.txt", "r").read()
 
-bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-slash = SlashCommand(bot, sync_commands=True)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+slash = SlashCommand(bot)
 guild_ids = []
 
 
@@ -22,12 +22,32 @@ async def on_message(message):
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'({channel[:10]}) - {username}: {user_message}')
+    if message.author == bot.user:
+        return
+    await bot.process_commands(message)
 
 
-@slash.slash(name="test", description="this is to test slash commands", guild_ids=guild_ids)  # TODO: GODO FIX!
-async def test(ctx: SlashContext):
-    print("Received")
+@bot.command(name='test')
+async def test(ctx, arg=''):
+    print("Received test command")
+    await ctx.send(f"test: {arg}")
+
+
+@bot.command(name='name')
+async def name(ctx):
+    await ctx.send(f"your name is {ctx.author}")
+
+
+@slash.slash(name="test2", description="this is to test slash commands", guild_ids=guild_ids)  # TODO: GODO FIX!
+async def test2(ctx: SlashContext):
+    print("Received slash command")
     await ctx.send(content="Success!")
+
+
+@slash.slash(name="newtest", description="this is to test slash commands, again", guild_ids=guild_ids)
+async def newtest(ctx: SlashContext):
+    print("Received slash command")
+    await ctx.send(content="Success in new!")
 
 
 bot.run(TOKEN)
